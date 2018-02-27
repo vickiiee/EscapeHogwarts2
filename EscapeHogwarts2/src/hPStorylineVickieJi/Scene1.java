b255334939f51b1ba58ae9d41ce8c04b6c636c62
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-02-27 14:57:35 -0500

    scene

--- EscapeHogwarts2/src/hPStorylineVickieJi/Scene1.java
+++ EscapeHogwarts2/src/hPStorylineVickieJi/Scene1.java
@@ -1,5 +1,20 @@
 package hPStorylineVickieJi;
 
-public class Scene1 {
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class Scene1 extends FullFunctionScreen{
+
+	public Scene1(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		
+	}
 
 }
