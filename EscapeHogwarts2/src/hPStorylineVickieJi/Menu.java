3fea24042654ea52a2958646090f63b2116ae1c0
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-27 15:07:43 -0500

    v

--- EscapeHogwarts2/src/hPStorylineVickieJi/Menu.java
+++ EscapeHogwarts2/src/hPStorylineVickieJi/Menu.java
@@ -1,5 +1,21 @@
 package hPStorylineVickieJi;
 
-public class Menu {
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class Menu extends FullFunctionScreen{
+
+	public Menu(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
