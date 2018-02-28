737427e01bdcc346ce046ed73e0ba0d961a85aee
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-02-28 15:19:15 -0500

    asd

---
+++ EscapeHogwarts2/src/hPLightsOutStephKev/GameLights.java
@@ -0,0 +1,21 @@
+package hPLightsOutStephKev;
+
+public class GameLights {
+
+	private int col;
+	private int row;
+
+	public GameLights(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+}
--- EscapeHogwarts2/src/hPLightsOutStephKev/GameScreen.java
+++ EscapeHogwarts2/src/hPLightsOutStephKev/GameScreen.java
@@ -1,5 +1,47 @@
 package hPLightsOutStephKev;
 
-public class GameScreen {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+import kevinStephRoom.KevinStephLight;
+
+public class GameScreen extends FullFunctionScreen {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private GameLights[][] board;
+	
+	public GameScreen(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		board = new GameLights[4][4];
+		createBoard();
+		
+		Graphic gamebg = new Graphic(0,0, getWidth(), getHeight(), "images/mirrorOfErised.jpg");
+		viewObjects.add(gamebg);
+		
+	}
+
+	public void createBoard() {
+		for(int row = 0; row < board.length; row++){
+			for(int col = 0; col < board[row].length; col++){
+				board[row][col] = new GameLights(row, col); //board is created 
+				board[row][col].lightOn(true); // all lights are on
+			}
+		}
+		
+	}
 
 }
