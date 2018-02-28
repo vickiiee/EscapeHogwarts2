fe496bf4ca43dc80be452a9a3c117ce867aca32e
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-28 14:27:18 -0500

    v

--- EscapeHogwarts2/src/hPStoryline/Menu.java
+++ EscapeHogwarts2/src/hPStoryline/Menu.java
@@ -1,19 +1,20 @@
-package hPStorylineVickieJi;
+package hPStoryline;
 
 import java.util.List;
 
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class Scene1 extends FullFunctionScreen{
+public class Menu extends FullFunctionScreen{
 
-	public Scene1(int width, int height) {
+	public Menu(int width, int height) {
 		super(width, height);
 		setVisible(true);
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
 		
 	}
 
