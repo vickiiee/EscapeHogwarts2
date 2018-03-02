032fc8648df16091fcb5a8de4d7b28821756e717
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-02 15:09:08 -0500

    connect four screen

---
+++ EscapeHogwarts2/src/hPConnect4Areej/ConnectFourAreej.java
@@ -0,0 +1,25 @@
+package hPConnect4Areej;
+
+import java.util.List;
+
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class ConnectFourAreej extends FullFunctionScreen {
+
+	private Graphic board;
+	
+	public ConnectFourAreej(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		board = new Graphic(300,300, getWidth(), getHeight(), "images/night.png");
+		viewObjects.add(board);
+		
+	}
+
+}
