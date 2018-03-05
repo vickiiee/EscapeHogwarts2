32959a3d6a28bcf6c501840dc6567dd04b4cd7c1
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-03-05 14:30:35 -0500

    h

--- EscapeHogwarts2/src/hPStoryline/Scene1.java
+++ EscapeHogwarts2/src/hPStoryline/Scene1.java
@@ -1,5 +1,6 @@
 package hPStoryline;
 
+import java.awt.Color;
 import java.util.List;
 
 import guiTeacher.components.Action;
@@ -7,15 +8,19 @@ import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
+import hPConnect4Areej.ConnectFourAreej;
+import hPMagicSquaresVickieJi.VickieHPFrontEnd;
 
 public class Scene1 extends FullFunctionScreen{
 
 	private Graphic background;
-	private Button stephStory;
+	private Button magicSquares;
+	private Button connectFour;
 
 	public Scene1(int width, int height) {
 		super(width, height);
 		setVisible(true);
+		magicSquares.setForeground(Color.LIGHT_GRAY);
 	}
 
 	@Override
@@ -23,14 +28,23 @@ public class Scene1 extends FullFunctionScreen{
 		background = new Graphic(0, 0, getWidth(), getHeight(), "images/castle.jpg");
 		viewObjects.add(background);
 		
-		stephStory = new Button(200,200,200,200,"enter", new Action() {
+		magicSquares = new Button(100, 450, 300, 75, "Magic Squares", new Action() {
 			
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				GuiLoadingVickie.loading.setScreen(new VickieHPFrontEnd(getWidth(), getHeight()));
 			}
 		});
+		viewObjects.add(magicSquares);
+		
+		connectFour = new Button(100, 550, 200, 75, "Connect Four", new Action() {
+			
+			@Override
+			public void act() {
+				GuiLoadingVickie.loading.setScreen(new ConnectFourAreej(getWidth(), getHeight()));
+			}
+		});
+		viewObjects.add(connectFour);
 	}
 
 }
