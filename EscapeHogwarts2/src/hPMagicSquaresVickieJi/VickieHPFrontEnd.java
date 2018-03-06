9d06f81dcccb62f709f8d0644c20542d65f27c48
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-03-06 15:23:41 -0500

    v

--- EscapeHogwarts2/src/hPMagicSquaresVickieJi/VickieHPFrontEnd.java
+++ EscapeHogwarts2/src/hPMagicSquaresVickieJi/VickieHPFrontEnd.java
@@ -34,6 +34,11 @@ public class VickieHPFrontEnd extends FullFunctionScreen{
 	private Button eightG;
 	private Button nineG;
 	
+	private String numClicked;
+	private int boxClicked;
+	
+	private TextArea error;
+	
 	public VickieHPFrontEnd(int width, int height) {
 		super(width, height);
 		setVisible(true);
@@ -41,7 +46,9 @@ public class VickieHPFrontEnd extends FullFunctionScreen{
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		numClicked = "0";
 		Color back =newColorWithAlpha(Color.white, 70);
+		
 		background = new Graphic(0, 0, getWidth(), getHeight(), "images/background3.jpg");
 		viewObjects.add(background);
 		
@@ -51,203 +58,211 @@ public class VickieHPFrontEnd extends FullFunctionScreen{
 		Graphic border = new Graphic(450,50,700,700, "images/grid.png");
 		viewObjects.add(border);
 		
+		error = new TextArea(50, 50, 200,200, "ERROR: \n Please choose a # button");
+		viewObjects.add(error);
+		error.setVisible(false);
+		
 // buttons 
 		one = new Button(50, 300, 100,100, "1", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
-			}
-			
+				numClicked = one.getText();
+				one.setBackground(Color.white);
+				error.setVisible(false);
+			}	
 		});
 		viewObjects.add(one);
 		
 		two = new Button(160, 300, 100,100, "2", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = two.getText();
+				two.setBackground(Color.white);
+				error.setVisible(false);
 			}
-			
 		});
 		viewObjects.add(two);
 		
 		three = new Button(270, 300, 100,100, "3", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = three.getText();
+				three.setBackground(Color.white);
+				error.setVisible(false);
 			}
-			
 		});
 		viewObjects.add(three);
 		
 		four = new Button(50, 410, 100,100, "4", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
-			}
-			
+				numClicked = four.getText();
+				four.setBackground(Color.white);
+				error.setVisible(false);
+			}	
 		});
 		viewObjects.add(four);
 		
 		five = new Button(160, 410, 100,100, "5", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				five.setVisible(false);
+				numClicked = five.getText();
+				five.setBackground(Color.white);
+				five.setVisible(true);
+				error.setVisible(true);
 			}
-			
 		});
 		viewObjects.add(five);
 		
 		six = new Button(270, 410, 100,100, "6", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = six.getText();
+				six.setBackground(Color.white);
+				error.setVisible(false);
 			}
-			
 		});
 		viewObjects.add(six);
 		
 		seven = new Button(50, 520, 100,100, "7", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = seven.getText();
+				seven.setBackground(Color.white);
+				error.setVisible(false);
 			}
 			
 		});
 		viewObjects.add(seven);
 		
 		eight = new Button(160, 520, 100,100, "8", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = eight.getText();
+				eight.setBackground(Color.white);
+				error.setVisible(false);
 			}
-			
 		});
 		viewObjects.add(eight);
 		
 		nine = new Button(270, 520, 100,100, "9", back, new Action() {
-
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				numClicked = nine.getText();
+				nine.setBackground(Color.white);
+				error.setVisible(false);
 			}
-			
 		});
 		viewObjects.add(nine);
 	
 //buttons on grid
-		oneG = new Button(500, 100, 216,216, "?", back, new Action() {
-
+		oneG = new Button(500, 100, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					oneG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(oneG);
 		
-		twoG = new Button(160, 300, 100,100, "?", back, new Action() {
-
+		twoG = new Button(702, 100, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					twoG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(twoG);
 		
-		threeG = new Button(270, 300, 100,100, "?", back, new Action() {
-
+		threeG = new Button(905, 100, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					threeG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(threeG);
 		
-		fourG = new Button(50, 410, 100,100, "?", back, new Action() {
+		fourG = new Button(500, 302, 195,195, "?", back, new Action() {
 
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					fourG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(fourG);
 		
-		fiveG = new Button(160, 410, 100,100, "?", back, new Action() {
-
+		fiveG = new Button(727, 327, 145,145, "?", Color.white, new Action() { //702, 302 195,195
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					fiveG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(fiveG);
 		
-		sixG = new Button(270, 410, 100,100, "?", back, new Action() {
-
+		sixG = new Button(905, 302, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					sixG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(sixG);
 		
-		sevenG = new Button(50, 520, 100,100, "?", back, new Action() {
-
+		sevenG = new Button(500, 505, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					sevenG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(sevenG);
-		
-		eightG = new Button(160, 520, 100,100, "?", back, new Action() {
 
+		eightG = new Button(702, 505, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					eightG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(eightG);
 		
-		nineG = new Button(270, 520, 100,100, "?", back, new Action() {
-
+		nineG = new Button(905, 505, 195,195, "?", back, new Action() {
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				if(numClicked.equals("0")) {
+					error.setVisible(true);
+				}else {
+					nineG.setText(numClicked);
+				}
 			}
-			
 		});
 		viewObjects.add(nineG);
 		
