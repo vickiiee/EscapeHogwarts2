65c1a6bca20f543328e307905f7194648e940935
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-04-13 15:19:49 -0400

    stairsssss

--- EscapeHogwarts2/src/hPStoryline/Storyline.java
+++ EscapeHogwarts2/src/hPStoryline/Storyline.java
@@ -24,7 +24,7 @@ public class Storyline extends FullFunctionScreen {
 		load.setSize(30);
 		quit.setSize(30);
 		setVisible(true);
-	}
+	} 
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
