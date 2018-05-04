47df92e8bf9e4bd17b36682cde524c121bdde699
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-05-04 15:23:41 -0400

    c

--- EscapeHogwarts2/src/hPCatchingGameJi/CatchingGameJi.java
+++ EscapeHogwarts2/src/hPCatchingGameJi/CatchingGameJi.java
@@ -8,6 +8,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import guiTeacher.components.Action;
+import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
@@ -27,6 +28,7 @@ public class CatchingGameJi extends FullFunctionScreen{
 	private int yPos = 0;
 
 	ArrayList<Object> potionsList = new ArrayList<Object>();
+	private Button testBtn;
 
 	public CatchingGameJi(int width, int height) {
 		super(width, height);
@@ -68,49 +70,40 @@ public class CatchingGameJi extends FullFunctionScreen{
 	public void generatePotion() {
 		chooseStart();
 		chooseTime();
-		potion = new Potion(xPos, 0, choosePotion());
-		viewObjects.add(potion);
-		potionsList.add(potion);
-		potion.move(xPos, 780, time);
-		potion.setAction(new Action() {
-
+		potion = new Potion(xPos, 0, choosePotion(), new Action() {
+			
+			/**
+			 * remove potion
+			 */
 			@Override
 			public void act() {
 				potion.setVisible(false);
 				potionsList.remove(potion);
 				viewObjects.remove(potion);
+				System.out.println("??");
 			}
 		});
-		checkPotionCaught();
-		/*
-		void mouseClicked(Object potion) {
-			potion.setVisible(false);
-			potionsList.remove(potion);
-			viewObjects.remove(potion);
-
-		}
-		*/
-	}
-	
-	
-	
-	public void testGenPotion() {
-		potion = new Potion(100, 100, choosePotion());
 		viewObjects.add(potion);
-		potion.setAction(new Action() {
+		potionsList.add(potion);
+		potion.move(xPos, 780, time);
+		
+		checkPotionCaught();
+	}
+		
+	public void testGenPotion() {
+		potion = new Potion(100, 100, choosePotion(), new Action() {
 			
 			@Override
 			public void act() {
 				potion.setVisible(false);
+				potionsList.remove(potion);
 				viewObjects.remove(potion);
+				System.out.println("??");
 			}
 		});
+		viewObjects.add(potion);
 	}
-
-	//action on potion
-	//if clicked
-	//set potion null: remove from arraylist & remove it from screen
-
+	
 	public void checkPotionCaught() {
 		if(potion.getHeight() == 780) {
 			if(potion != null) {
@@ -170,5 +163,15 @@ public class CatchingGameJi extends FullFunctionScreen{
 
 		gameStatus = new TextArea(300, 300, 500, 100, "");
 		viewObjects.add(gameStatus);
+		
+		testBtn = new Button(500, 100, 200, 100, "remove", new Action() {
+			
+			@Override
+			public void act() {
+				viewObjects.remove(potion);
+				
+			}
+		});
+		viewObjects.add(testBtn);
 	}
 }
--- EscapeHogwarts2/src/hPCatchingGameJi/Potion.java
+++ EscapeHogwarts2/src/hPCatchingGameJi/Potion.java
@@ -7,8 +7,9 @@ import guiTeacher.components.ClickableGraphic;
 
 public class Potion extends ClickableGraphic{
 
-	public Potion(int x, int y, String imageLocation) {
+	public Potion(int x, int y, String imageLocation, Action a) {
 		super(x, y, imageLocation);
+		setAction(a);
 		// TODO Auto-generated constructor stub
 	}
 
