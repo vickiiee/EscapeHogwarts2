7fb9b5077ca9322bc1ed1319f6c7a9acf73e82ac
BT_1N3_06 <jwu4227@bths.edu>
2018-05-29 15:19:51 -0400

    game

--- EscapeHogwarts2/src/hPConnect4Areej/ConnectFourAreej.java
+++ EscapeHogwarts2/src/hPConnect4Areej/ConnectFourAreej.java
@@ -9,7 +9,8 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 public class ConnectFourAreej extends FullFunctionScreen {
 
 	private Graphic background;
-	private Graphic back;
+	private Graphic player;
+	private Graphic comp;
 	private Graphic board;
 	private Graphic galleon;
 	
@@ -24,8 +25,19 @@ public class ConnectFourAreej extends FullFunctionScreen {
 		background = new Graphic(0,0, getWidth(), getHeight(), "images/party.jpg");
 		viewObjects.add(background);
 		
-		back = new Graphic(350,220, 600, 620, "images/mSCBoard.jpg");
-		//viewObjects.add(back);
+		for(int i = 360; i<900; i+=85) {
+			for(int j = 225; j < 700 ; j+=85) {
+				player = new Graphic(i,j, 85, 85, "images/pp.gif");
+				viewObjects.add(player);
+			}
+		}
+		
+		for(int i = 360; i<900; i+=85) {
+			for(int j = 225; j < 700 ; j+=85) {
+				comp = new Graphic(i,j, 85, 85, "images/cp.gif");
+				viewObjects.add(comp);
+			}
+		}
 		
 		board = new Graphic(300,200, 700, getHeight(), "images/cboard.png");
 		viewObjects.add(board);
