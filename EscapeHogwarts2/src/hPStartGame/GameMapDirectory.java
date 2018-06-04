b4deb4d4711f50afaadf8389bd1338bb092587ae
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-06-04 15:18:25 -0400

    c

--- EscapeHogwarts2/src/hPStartGame/GameMapDirectory.java
+++ EscapeHogwarts2/src/hPStartGame/GameMapDirectory.java
@@ -2,6 +2,8 @@ package hPStartGame;
 
 import java.util.List;
 
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
 import guiTeacher.components.ClickableGraphic;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.TextArea;
@@ -15,6 +17,10 @@ public class GameMapDirectory extends FullFunctionScreen{
 	private Graphic pointer2;
 	private Graphic pointer3;
 	private TextArea sampletxt;
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	private boolean menuRunning = true;
 	
 	public GameMapDirectory(int width, int height) {
 		super(width, height);
@@ -22,6 +28,32 @@ public class GameMapDirectory extends FullFunctionScreen{
 	
 	public boolean isHovered(int x, int y) {
 		return x > getX() && x < getX() + getWidth() && y > getY() && y < getY() + getHeight();
+	}
+	
+	public void menuRun() {
+		while(menuRunning) {
+			if(isHovered(900, 300)) {
+				button1.setText("FOREST");
+				System.out.println("working");
+			}else {
+				button1.setText("empty");
+				System.out.println("N/A");
+				
+			}
+			
+			if(isHovered(750,670)) {
+				button2.setText("working");
+			}else {
+				button2.setText("empty");
+			}
+			
+			if(isHovered(1000,100)) {
+				button3.setText("working");
+			}else {
+				button3.setText("empty");
+			}
+		}
+		
 	}
 
 	@Override
@@ -31,20 +63,45 @@ public class GameMapDirectory extends FullFunctionScreen{
 		
 		pointer1 = new Graphic(900, 300, 30, 40, "images/pointer.png");
 		viewObjects.add(pointer1);
+		button1 = new Button(1000, 100, 30, 40, "a", new Action() {
+			//forest
+			@Override
+			public void act() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		viewObjects.add(button1);
 		
 		pointer2 = new Graphic(750, 670, 30, 40, "images/pointer.png");
 		viewObjects.add(pointer2);
+		button2 = new Button(900, 290, 30, 40, "a", new Action() {
+			//hogwarts
+			@Override
+			public void act() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		viewObjects.add(button2);
 		
-		pointer3 = new ClickableGraphic(1000, 100, 30, 40, "images/pointer.png");
+		pointer3 = new Graphic(1000, 100, 30, 40, "images/pointer.png");
 		viewObjects.add(pointer3);
-		/*
-		sampletxt = new TextArea(1000, 90, 100, 100, "");
-		viewObjects.add(sampletxt);
+		button3 = new Button(750, 670, 30, 40, "a", new Action() {
+			//train
+			@Override
+			public void act() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		viewObjects.add(button3);
 		
-		if(isHovered(30,40)) {
-			sampletxt.setText("Catching Game");
+		if(isHovered(900, 300)) {
+			button1.setText("HARRY");
+		}else {
+			button1.setText("");
 		}
-		*/
 	}
 
 }
