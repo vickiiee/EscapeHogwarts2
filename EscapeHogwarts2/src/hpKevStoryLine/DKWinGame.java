3cba6ca70d3a261b26b143bbb9af726ee3efcdde
Kevin Liu <Kevin Liu@DESKTOP-41M1TEJ.fios-router.home>
2018-06-21 12:32:19 -0400

    qwe

--- EscapeHogwarts2/src/hpKevStoryLine/DKWinGame.java
+++ EscapeHogwarts2/src/hpKevStoryLine/DKWinGame.java
@@ -29,7 +29,8 @@ public class DKWinGame extends FullFunctionScreen {
 	private Graphic hermione;
 	
 	private String[] nameList = {"Harry", "Ron", "Hermione"};
-	private String[] dialogList = {"What is one doing so close to Hogwarts?", };
+	private String[] dialogList = {"What in the world was that?", "We've seen ghosts before; that definitely isn't one.", "What is one doing so close to Hogwarts?"};
+	private String[] ansList = {"This ghost is different", "I was merely testing you", ""};
 	
 	public DKWinGame(int width, int height) {
 		super(width, height);
@@ -43,7 +44,10 @@ public class DKWinGame extends FullFunctionScreen {
 	
 	public void initAllObjects(List<Visible> viewObjects) {
 		
-		harry = new Graphic(700,260,400,400, "charHarry1.png");
+		backImg = new Graphic(0, 0, getWidth(), getHeight(), "kimg/carriage.jpg");
+		viewObjects.add(backImg);
+		
+		harry = new Graphic(700,260,400,400, "images/charHarry1.png");
 		viewObjects.add(harry);
 		harry.setVisible(false);
 		
@@ -55,11 +59,6 @@ public class DKWinGame extends FullFunctionScreen {
 		viewObjects.add(hermione);
 		hermione.setVisible(false);
 		
-		backImg = new Graphic(0, 0, getWidth(), getHeight(), "kimg/carriage.jpg");
-		viewObjects.add(backImg);
-		
-		
-		
 		textBox = new Graphic(80,80,1200,700, "simg/textbox.png");
 		viewObjects.add(textBox);
 		
@@ -69,20 +68,38 @@ public class DKWinGame extends FullFunctionScreen {
 		dialog = new TextArea(180,590,1000,500,"What in the world was that?");
 		viewObjects.add(dialog);
 		
-		buttonOne = new Button(850, 365, 325, 105, "It was merely a ghost", Color.black, null);
+		buttonOne = new Button(125, 225, 350, 105, "It was merely a ghost", Color.LIGHT_GRAY, null);
 		
 		buttonOne.setAction(new Action() {
 			
 			@Override
 			public void act() {
+				if(dialog.getText().equals(dialogList[0])) {
+					dialog.setText(dialogList[1]);
+					name.setText(nameList[2]);
+					buttonOne.setText(ansList[0]);
+					buttonTwo.setText(ansList[1]);
+				}
 				
-				
+				if(name.getText().equals(nameList[0])) {
+					harry.setVisible(true);
+					ron.setVisible(false);
+					hermione.setVisible(false);
+				}else if(name.getText().equals(nameList[1])){
+					harry.setVisible(false);
+					ron.setVisible(true);
+					hermione.setVisible(false);
+				}else {
+					harry.setVisible(false);
+					ron.setVisible(false);
+					hermione.setVisible(true);
+				}
 			}
 		});
 		
 		viewObjects.add(buttonOne);
 		
-		buttonTwo = new Button(850,475,325,105, "That was a Dementor", Color.black, null);
+		buttonTwo = new Button(125,345,350,105, "That was a Dementor", Color.LIGHT_GRAY, null);
 		
 		buttonTwo.setAction(new Action() {
 			
